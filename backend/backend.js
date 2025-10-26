5e2f871d0187c965654b94abde77dad022fa5e7c
Rachana Raju <rachanaraju03@gmail.com>
2025-10-26 13:58:49 -0700

    post art endpoint

--- backend/backend.js
+++ backend/backend.js
@@ -1,4 +1,5 @@
 import express from "express";
+import artServices from "./models/art-services.js";
 
 const app = express();
 const port = 8000;
@@ -7,6 +8,29 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/art", (req, res) => {
+  const artToAdd = req.body;
+  console.log(artToAdd)
+  if (!artToAdd["title"] || artToAdd["title"] === "" ){
+    console.log("Missing title");
+    res.status(400).send("Missing title")
+  }
+  if (!artToAdd["picture"] || artToAdd["picture"] === ""){
+    console.log("Missing picture");
+    res.status(400).send("Missing picture")
+  }
+  if (!artToAdd["measurements"]["height"]|| !artToAdd["measurements"]["width"]){
+    console.log("Missing measurements");
+    res.status(400).send("Missing measurements")
+  }
+  artServices.addArt(artToAdd).then(
+      (result) => res.status(201).send(result)
+  ).catch((error)=>{
+      console.log(error);
+      res.status(500).end();
+  })
 });
 
 app.listen(port, () => {
---
+++ backend/models/art-services.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import artModel from './art.js';
+dotenv.config();
+
+mongoose.set("debug", true);
+
+mongoose.connect(process.env.MONGO_URI,{}).catch((error)=>console.log(error));
+
+function addArt(art){
+    const artToAdd = new artModel(art);
+    const promise = artToAdd.save();
+    return promise;
+}
+
+export default {
+    addArt,
+};
