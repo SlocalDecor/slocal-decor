87b13cd5374fa67f6df50033ded6d504a390f855
Rachana Raju <rachanaraju03@gmail.com>
2025-11-02 22:35:37 -0800

    home page component

---
+++ frontend/src/HomePage.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import "./style.css";
+
+function HomePage() {
+    return (
+        <div className="user-page">
+          {/* Placeholder top bar */}
+          <div className="top-bar">
+            <p>TODO: Add nav bar</p>
+          </div>
+    
+          <div className="hero">
+            <div className="overlay"/>
+            <h1 className="main-text">slocal decor</h1>
+            <p className="sub-text">Some nice tagline</p>
+          </div>
+          
+        </div>
+      );
+}
+
+export default HomePage;
