66a9fb2c586bd09f7e47d9ca9bcb383d0f5a6321
NatalieRusin <nrusin@calpoly.edu>
2025-11-05 14:44:57 -0800

    new arrivals

--- frontend/src/components/UserLogin.jsx
+++ frontend/src/components/UserLogin.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "../style.css";
 
 export default function UserLogin() {
   const [username, setUsername] = useState("");
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import UserLogin from "./components/UserLogin.jsx";
+import NewArrivals from "./components/NewArrivals.jsx";
+//import UserLogin from "./components/UserLogin.jsx";
 //import UserPage from "./components/UserProfile.jsx";
 //import HomePage from "./components/HomePage";
 import "./style.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <UserLogin />
+    <NewArrivals />
+    {/* <UserLogin /> */}
     {/* <HomePage /> */}
     {/* <UserPage /> */}
   </React.StrictMode>
