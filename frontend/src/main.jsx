4a75b9f853c3fc17efbe4a851eb696886d7f14b2
Rachana Raju <rachanaraju03@gmail.com>
2025-11-06 16:20:06 -0800

    saved items

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import UserPage from "./components/UserProfile.jsx";
 import HomePage from "./components/HomePage";
 import SignUp from "./components/SignUp.jsx";
+import SavedItems from "./components/SavedItems.jsx";
 import "./style.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <SignUp />
+    <SavedItems/>
+    {/* <SignUp /> */}
     {/*<UserPage />*/}
     {/* <HomePage /> */}
   </React.StrictMode>
