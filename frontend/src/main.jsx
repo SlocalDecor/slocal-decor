26318cdcf7511a2d57495fa4baf184f9bbc7a766
NatalieRusin <nrusin@calpoly.edu>
2025-11-10 16:35:20 -0800

    sticky

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,10 +7,12 @@ import NewArrivals from "./components/NewArrivals.jsx";
 import SavedItems from "./components/SavedItems.jsx";
 import "./style.css";
 import UserLogin from "./components/UserLogin.jsx";
+import NavBar from "./components/NavBar.jsx";
+import UserProfile from "./components/UserProfile.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <SignUp />
+    <HomePage />
     {/*<UserPage />*/}
     {/* <HomePage /> */}
     {/* <UserPage /> */}
