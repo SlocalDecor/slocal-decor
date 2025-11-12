ae878e52d37af7cbf3bd90b1c3ea92cb492852e3
NatalieRusin <nrusin@calpoly.edu>
2025-11-11 19:21:46 -0800

    main-back

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import NewItem from "./components/NewItem.jsx";
-//import NewArrivals from "./components/NewArrivals.jsx";
-//import UserLogin from "./components/UserLogin.jsx";
-//import UserPage from "./components/UserProfile.jsx";
-//import HomePage from "./components/HomePage";
+import UserPage from "./components/UserProfile.jsx";
+import HomePage from "./components/HomePage";
+import SignUp from "./components/SignUp.jsx";
+import NewArrivals from "./components/NewArrivals.jsx";
+import SavedItems from "./components/SavedItems.jsx";
+import { BrowserRouter } from "react-router-dom";
+import App from "./components/MyApp.jsx";
 import "./style.css";
 import UserLogin from "./components/UserLogin.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <NewItem />
-    {/*<UserPage />*/}
-    {/* <HomePage /> */}
-    {/* <UserPage /> */}
-    {/* <NewArrivals/> */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
