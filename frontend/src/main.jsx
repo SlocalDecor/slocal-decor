c30258ffe9436857dd12e6bb7a9d79b175be0285
NatalieRusin <nrusin@calpoly.edu>
2025-11-16 21:16:19 -0800

    main back to normal

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,19 +1,3 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import UserLogin from "./components/UserLogin.jsx";
-// import SignUp from "./components/SignUp.jsx";
-// //import UserPage from "./components/UserProfile.jsx";
-// //import HomePage from "./components/HomePage";
-// import "./style.css";
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <SignUp />
-//     {/* <HomePage /> */}
-//     {/* <UserPage /> */}
-//   </React.StrictMode>
-// );
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import UserProfile from "./components/UserProfile.jsx";
