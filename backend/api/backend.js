999b6ec59dcc83f66144521c7a3ab9278fd28bc6
Sumedha Kundurthi <70816655+SumedhaKun@users.noreply.github.com>
2025-11-16 18:31:25 -0800

    moved api back

--- backend/api/backend.js
+++ backend/api/backend.js
@@ -1,4 +1,4 @@
 import serverless from "serverless-http";
-import app from "../backend/backend.js";
+import app from "../backend.js";
 
 export const handler = serverless(app);
--- backend/api/login.js
+++ backend/api/login.js
@@ -1,4 +1,4 @@
 import serverless from "serverless-http";
-import app from "../backend/backend.js"; // your Express app with routes
+import app from "../backend.js";
 
 export const handler = serverless(app);
