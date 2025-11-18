1d0cd7c5448fdebb4106810acad52941603ab8bc
Sumedha Kundurthi <70816655+SumedhaKun@users.noreply.github.com>
2025-11-17 22:51:27 -0800

    changed login handler

--- api/login.js
+++ api/login.js
@@ -1,5 +1,11 @@
 import serverless from "serverless-http";
 import app from "../backend/backend.js";
-import { connectDB } from "../backend/db.js";
+import { loginUser } from "../backend/auth.js";
 
-export default serverless(app);
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).send("Method Not Allowed");
+  }
+
+  await loginUser(req, res);
+}
