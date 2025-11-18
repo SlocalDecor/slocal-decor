2ec17b24fa77dc1a664247df003086d7c77aae13
Sumedha Kundurthi <70816655+SumedhaKun@users.noreply.github.com>
2025-11-17 21:59:56 -0800

    added necessary import

--- api/login.js
+++ api/login.js
@@ -1,5 +1,6 @@
 import serverless from "serverless-http";
 import app from "../backend/backend.js";
+import { connectDB } from "../backend/db.js";
 
 export default async function handler(req, res) {
   await connectDB(); // ensures DB is ready
