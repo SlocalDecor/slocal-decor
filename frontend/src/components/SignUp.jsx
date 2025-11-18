7ba0828d33dc0ccab95cc414ed81c699576c9dca
NatalieRusin <nrusin@calpoly.edu>
2025-11-18 12:46:34 -0800

    sigun up

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -10,7 +10,7 @@ export default function SignUp() {
     phone: "",
     password: "",
   });
-
+  //update
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({
