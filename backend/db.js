7bfc370579a9752fabfdc62dfba42aa86074eb89
Sumedha Kundurthi <70816655+SumedhaKun@users.noreply.github.com>
2025-11-17 23:07:45 -0800

    added routes to vercel.json

--- backend/db.js
+++ backend/db.js
@@ -2,20 +2,22 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-let isConnected = false;
+let cached = global.mongoose;
 
-export async function connectDB() {
-  if (isConnected) return;
+if (!cached) cached = global.mongoose = { conn: null, promise: null };
 
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      serverSelectionTimeoutMS: 5000,
-    });
-
-    isConnected = true;
-    console.log("MongoDB connected:", conn.connection.host);
-  } catch (err) {
-    console.error("Mongo DB connection error:", err);
-    throw err;
+export default async function connectDB() {
+  if (cached.conn) {
+    return cached.conn;
   }
+
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(process.env.MONGO_URI)
+      .then((mongoose) => {
+        return mongoose;
+      });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
