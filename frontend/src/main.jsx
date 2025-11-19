9c3c08cd9af5b7fea590e93e9236340357204a66
NatalieRusin <nrusin@calpoly.edu>
2025-11-19 13:34:42 -0800

    main-back-to-original-state

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,16 +11,6 @@ import App from "./components/MyApp.jsx";
 import "./style.css";
 import UserLogin from "./components/UserLogin.jsx";
 
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <NewItem />
-//     {/* <UserLogin /> */}
-//     {/* <HomePage /> */}
-//     {/* <UserPage /> */}
-//   </React.StrictMode>
-// );
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
