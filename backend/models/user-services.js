483b5a1525d27871c6a353cf8404bed54e864452
Sumedha Kundurthi <70816655+SumedhaKun@users.noreply.github.com>
2025-12-05 17:50:23 -0800

    added user tests

--- backend/models/user-services.js
+++ backend/models/user-services.js
@@ -3,6 +3,18 @@ import userModel from "./user.js";
 import artModel from "./art.js";
 
 function addUser(user) {
+  if (!user["name"] || user["name"] === "") {
+    throw new Error("Missing name");
+  }
+  if (!user["email"] || user["email"] === "") {
+    throw new Error("Missing email");
+  }
+  if (!user["phone"] || user["phone"] === "") {
+    throw new Error("Missing phone");
+  }
+  if (!/.+@.+\..+/.test(user["email"])) {
+    throw new Error("Bad email format");
+  }
   const userToAdd = new userModel(user);
   const promise = userToAdd.save();
   return promise;
@@ -27,6 +39,36 @@ function findUserForLogin(email) {
   return promise;
 }
 
+function findUserByEmail(email) {
+  const user = userModel.findOne({ email: email });
+  const promise = Promise.all([user]);
+  return promise;
+}
+
+function findUserByName(name) {
+  const user = userModel.findOne({ name: name });
+  const promise = Promise.all([user]);
+  return promise;
+}
+
+function updateUserById(userId, updateObj) {
+  const updatedUser = userModel.findByIdAndUpdate(userId, updateObj, {
+    new: true,
+  });
+  const promise = Promise.all([updatedUser]);
+  return promise;
+}
+
+function updateUserArt(newArtId, owner) {
+  const updatedUser = userModel.findByIdAndUpdate(
+    owner,
+    { $push: { postedArt: newArtId } },
+    { new: true }
+  );
+  const promise = Promise.all([updatedUser]);
+  return promise;
+}
+
 function getUsers() {
   const users = userModel.find({});
   const promise = Promise.all([users]);
@@ -37,22 +79,26 @@ function removeArtFromUser(userId, artId) {
   if (!userId || !artId) {
     throw new Error("userId and artId are required");
   }
-  return userModel.findByIdAndUpdate(
+  const updatedUser = userModel.findByIdAndUpdate(
     userId,
     { $pull: { postedArt: artId } },
     { new: true }
   );
+  const promise = Promise.all([updatedUser]);
+  return promise;
 }
 
 function unsaveArt(userId, artId) {
   if (!userId || !artId) {
     throw new Error("userId and artId are required");
   }
-  return userModel.findByIdAndUpdate(
+  const updatedUser = userModel.findByIdAndUpdate(
     userId,
     { $pull: { savedArt: artId } },
     { new: true }
   );
+  const promise = Promise.all([updatedUser]);
+  return promise;
 }
 
 export default {
@@ -63,4 +109,8 @@ export default {
   getUsers,
   removeArtFromUser,
   unsaveArt,
+  updateUserArt,
+  findUserByEmail,
+  findUserByName,
+  updateUserById,
 };
